5b8a3dcbff04ddec429d57c91f3bcc6f4dc62075
teahouse-dot <magundy@iu.edu>
2026-04-26 16:43:26 -0400

    Update streamlit_app.py
    removed 's' from streamlist, so it is now correctly streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 ## from https://docs.streamlit.io/develop/tutorials/databases/mysql
-import streamlist as st
+import streamlit as st
 ## initialize connection
 conn=st.connection('mysql', type='sql')
