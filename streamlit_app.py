f5d8121069f760e8daebe4ef7382feddd6ca03c9
teahouse-dot <magundy@iu.edu>
2026-05-01 19:59:09 -0400

    Update streamlit_app.py
    Matt - the last change broke the visualization.  Now trying to fix it.

--- streamlit_app.py
+++ streamlit_app.py
@@ -184,6 +184,6 @@ sleephealth = fetch_data("""SELECT sleep_desc, health_desc, COUNT(*) as count
 FROM health_fact JOIN sleep_fact ON health_fact.fact_id = sleep_fact.fact_id
 JOIN health_dim ON health_dim.health_id = health_fact.mental_health_id
 JOIN sleep_dim ON sleep_dim.sleep_id = sleep_fact.stress_sleep_id
-GROUP BY mental_health_id""")
+GROUP BY sleep_desc, mental_health_id""")
 
 st.line_chart(sleephealth, x="health_desc", y="count", color="sleep_desc")
