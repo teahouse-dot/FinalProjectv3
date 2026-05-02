b078c7e270a645d3e6fd68e5f7822487ef1f84cc
teahouse-dot <magundy@iu.edu>
2026-05-01 21:01:20 -0400

    Update streamlit_app.py
    Matt - I noticed in testing that the gender filter does not work on the visualization. I think this is because gender data is joined in the query, which comes from the demographics_fact table.  I'm adding a join on the primary key (fact_id) between the demographics_fact table and the health_fact table.  Hopefully this connects the filter that Jianghua created.
    Additional join: 
    JOIN demographics_fact ON health_fact.fact_id = demographics_fact.fact_id

--- streamlit_app.py
+++ streamlit_app.py
@@ -184,6 +184,7 @@ sleephealth = fetch_data("""SELECT sleep_desc, mental_health_id, health_desc, CO
 FROM health_fact JOIN sleep_fact ON health_fact.fact_id = sleep_fact.fact_id
 JOIN health_dim ON health_dim.health_id = health_fact.mental_health_id
 JOIN sleep_dim ON sleep_dim.sleep_id = sleep_fact.stress_sleep_id
+JOIN demographics_fact ON health_fact.fact_id = demographics_fact.fact_id
 WHERE mental_health_id <> -1
 GROUP BY sleep_desc, mental_health_id, health_desc
 ORDER BY mental_health_id DESC
