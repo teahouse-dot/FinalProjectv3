9b45c819646fdbf44ef9906d0579b9eef409d6d9
teahouse-dot <magundy@iu.edu>
2026-05-01 21:50:03 -0400

    Update streamlit_app.py
    Matt - now the ALL filter doesn't work, and neither does the Male or Female.  So I am removing this statement JOIN gender_dim g ON d.gender_id = g.gender_id temporarily.  I'm wondering if the double WHERE statements are interfering with each other.  Going to research that next.

--- streamlit_app.py
+++ streamlit_app.py
@@ -185,7 +185,6 @@ FROM health_fact JOIN sleep_fact ON health_fact.fact_id = sleep_fact.fact_id
 JOIN health_dim ON health_dim.health_id = health_fact.mental_health_id
 JOIN sleep_dim ON sleep_dim.sleep_id = sleep_fact.stress_sleep_id
 JOIN demographics_fact d ON health_fact.fact_id = demographics_fact.fact_id
-JOIN gender_dim g ON d.gender_id = g.gender_id
 WHERE mental_health_id <> -1
 {where_clause}
 GROUP BY sleep_desc, mental_health_id, health_desc
