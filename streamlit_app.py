74e0623bd2433a58d5b9102b7c023eeba0e2c42e
teahouse-dot <magundy@iu.edu>
2026-05-01 21:46:51 -0400

    Update streamlit_app.py
    Matt - The behavior of the filter is working when the gender is set to all, but when Male or Female is selected, then it retrieves an error.  This is suggesting to me that the filter selection is not matching with the gender_desc.  I looked back at Jianghua's filter and see that she is using an abbreviation to make the join.  I'm replicating that in my query: 
    JOIN demographics_fact d ON health_fact.fact_id = demographics_fact.fact_id
    JOIN gender_dim g ON d.gender_id = g.gender_id

--- streamlit_app.py
+++ streamlit_app.py
@@ -184,8 +184,8 @@ sleephealth = fetch_data(f"""SELECT sleep_desc, mental_health_id, health_desc, C
 FROM health_fact JOIN sleep_fact ON health_fact.fact_id = sleep_fact.fact_id
 JOIN health_dim ON health_dim.health_id = health_fact.mental_health_id
 JOIN sleep_dim ON sleep_dim.sleep_id = sleep_fact.stress_sleep_id
-JOIN demographics_fact ON health_fact.fact_id = demographics_fact.fact_id
-JOIN gender_dim ON demographics_fact.gender_id = gender_dim.gender_id
+JOIN demographics_fact d ON health_fact.fact_id = demographics_fact.fact_id
+JOIN gender_dim g ON d.gender_id = g.gender_id
 WHERE mental_health_id <> -1
 {where_clause}
 GROUP BY sleep_desc, mental_health_id, health_desc
