fd5849bb88c6e168fe002b0a333f516af4a9a143
teahouse-dot <magundy@iu.edu>
2026-05-01 21:08:40 -0400

    Update streamlit_app.py
    Matt - The filter did not work because I only joined the fact tables.  I suspected it would not automatically connect to the gender data because the filter is referencing the desc value.  I am adding a secondary join to the dimensional table where the gender description exists.
    
    JOIN gender_dim ON demographics_fact.gender_id = gender_dim.gender_id

--- streamlit_app.py
+++ streamlit_app.py
@@ -185,6 +185,7 @@ FROM health_fact JOIN sleep_fact ON health_fact.fact_id = sleep_fact.fact_id
 JOIN health_dim ON health_dim.health_id = health_fact.mental_health_id
 JOIN sleep_dim ON sleep_dim.sleep_id = sleep_fact.stress_sleep_id
 JOIN demographics_fact ON health_fact.fact_id = demographics_fact.fact_id
+JOIN gender_dim ON demographics_fact.gender_id = gender_dim.gender_id
 WHERE mental_health_id <> -1
 GROUP BY sleep_desc, mental_health_id, health_desc
 ORDER BY mental_health_id DESC
